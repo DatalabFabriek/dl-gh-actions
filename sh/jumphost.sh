d5b10701edaffc32d6ada18417e843f380c57d66
Harmen van der Veer <harmen_github_com@xtremesf.nl>
2022-10-10 13:29:04 +0200

    Made it into a function

--- sh/jumphost.sh
+++ sh/jumphost.sh
@@ -23,56 +23,73 @@
 #EOF
 #)
 
-# Remove current SSH config file
-rm -f ~/.ssh/config && \
-    mkdir -p ~/.ssh/ && \
-    touch ~/.ssh/config && \
-    rm ~/dljumphost_identity_*.txt 
+dl_jumphost_make_configfile() {
+    # Remove current SSH config file
 
-# Loop through any & all jumphosts and configure them
-i=1
-varname="DL_JUMPHOST_CONN_$i"
+    if [ -f ~/.ssh/config ]; then
+        mv ~/.ssh/config ~/.ssh/config.back
+    fi
 
-while [[ "${!varname}" != "" ]]
-do
-    # Extract info from connection string
-    user=$(echo "${!varname}" | sed -E 's/(.*)@(.*)\:([0-9]*)/\1/')
-    host=$(echo "${!varname}" | sed -E 's/(.*)@(.*)\:([0-9]*)/\2/')
-    port=$(echo "${!varname}" | sed -E 's/(.*)@(.*)\:([0-9]*)/\3/')
+    rm -f ~/.ssh/config && \
+        mkdir -p ~/.ssh/ && \
+        touch ~/.ssh/config && \
+        rm ~/dljumphost_identity_*.txt 
 
-    # Create config file entry for this jumphost
-    CONFIG="Host dljumpproxy$i"$'\n\t'"HostName $host"$'\n\t'"User $user"$'\n\t'"Port $port"$'\n\t'"IdentityFile ~/dljumphost_identity_$i.txt"$'\n\t'"StrictHostKeyChecking no"$'\n\t'"UserKnownHostsFile=/dev/null"$'\n\t'"ForwardAgent yes"
+    # Loop through any & all jumphosts and configure them
+    i=1
+    varname="DL_JUMPHOST_CONN_$i"
+
+    while [[ "${!varname}" != "" ]]
+    do
+        # Extract info from connection string
+        user=$(echo "${!varname}" | sed -E 's/(.*)@(.*)\:([0-9]*)/\1/')
+        host=$(echo "${!varname}" | sed -E 's/(.*)@(.*)\:([0-9]*)/\2/')
+        port=$(echo "${!varname}" | sed -E 's/(.*)@(.*)\:([0-9]*)/\3/')
+
+        # Create config file entry for this jumphost
+        CONFIG="Host dljumpproxy$i"$'\n\t'"HostName $host"$'\n\t'"User $user"$'\n\t'"Port $port"$'\n\t'"IdentityFile ~/dljumphost_identity_$i.txt"$'\n\t'"StrictHostKeyChecking no"$'\n\t'"UserKnownHostsFile=/dev/null"$'\n\t'"ForwardAgent yes"
+
+        if [ $i -gt 1 ]; then
+            CONFIG="$CONFIG"$'\n\t'"ProxyJump dljumpproxy$(($i-1))"
+        fi
+
+        echo "$CONFIG" >> ~/.ssh/config
+
+        # Write key to file
+        varname_key="DL_JUMPHOST_SSHKEY_$i"
+        echo "${!varname_key}" > ~/dljumphost_identity_$i.txt
+        chmod 600 ~/dljumphost_identity_$i.txt
+
+        # Prepare for next loop
+        i=$(($i+1))
+        varname="DL_JUMPHOST_CONN_$i"
+    done
+
+    # Target host
+    user=$(echo "$DL_DEPLOYHOST_CONN" | sed -E 's/(.*)@(.*)\:([0-9]*)/\1/')
+    host=$(echo "$DL_DEPLOYHOST_CONN" | sed -E 's/(.*)@(.*)\:([0-9]*)/\2/')
+    port=$(echo "$DL_DEPLOYHOST_CONN" | sed -E 's/(.*)@(.*)\:([0-9]*)/\3/')
+
+    CONFIG="Host dltarget"$'\n\t'"HostName $host"$'\n\t'"User $user"$'\n\t'"Port $port"$'\n\t'"IdentityFile ~/dljumphost_identity_target.txt"$'\n\t'"StrictHostKeyChecking no"$'\n\t'"UserKnownHostsFile=/dev/null"$'\n\t'"ForwardAgent yes"
 
     if [ $i -gt 1 ]; then
         CONFIG="$CONFIG"$'\n\t'"ProxyJump dljumpproxy$(($i-1))"
     fi
 
+    # Add target to SSH config, add key
     echo "$CONFIG" >> ~/.ssh/config
+    echo -e "$DL_DEPLOYHOST_SSHKEY" > ~/dljumphost_identity_target.txt
+    chmod 600 ~/dljumphost_identity_target.txt
 
-    # Write key to file
-    varname_key="DL_JUMPHOST_SSHKEY_$i"
-    echo "${!varname_key}" > ~/dljumphost_identity_$i.txt
-    chmod 600 ~/dljumphost_identity_$i.txt
+    # If you use `ssh dltarget`, then you should be inside the target host without further ado, routed through all jumphosts.
+}
 
-    # Prepare for next loop
-    i=$(($i+1))
-    varname="DL_JUMPHOST_CONN_$i"
-done
+dl_jumphost_restore_configfile() {
+    if [ -f ~/.ssh/config.back ]; then
+        mv ~/.ssh/config.back ~/.ssh/config
+    else
+        rm ~/.ssh/config
+    fi
 
-# Target host
-user=$(echo "$DL_DEPLOYHOST_CONN" | sed -E 's/(.*)@(.*)\:([0-9]*)/\1/')
-host=$(echo "$DL_DEPLOYHOST_CONN" | sed -E 's/(.*)@(.*)\:([0-9]*)/\2/')
-port=$(echo "$DL_DEPLOYHOST_CONN" | sed -E 's/(.*)@(.*)\:([0-9]*)/\3/')
-
-CONFIG="Host dltarget"$'\n\t'"HostName $host"$'\n\t'"User $user"$'\n\t'"Port $port"$'\n\t'"IdentityFile ~/dljumphost_identity_target.txt"$'\n\t'"StrictHostKeyChecking no"$'\n\t'"UserKnownHostsFile=/dev/null"$'\n\t'"ForwardAgent yes"
-
-if [ $i -gt 1 ]; then
-    CONFIG="$CONFIG"$'\n\t'"ProxyJump dljumpproxy$(($i-1))"
-fi
-
-# Add target to SSH config, add key
-echo "$CONFIG" >> ~/.ssh/config
-echo -e "$DL_DEPLOYHOST_SSHKEY" > ~/dljumphost_identity_target.txt
-chmod 600 ~/dljumphost_identity_target.txt
-
-# If you use `ssh dltarget`, then you should be inside the target host without further ado, routed through all jumphosts.
+    rm ~/dljumphost_identity_*
+}
